39ffc49179d34226ea3d21a44759d15241fc37e2
mourisl <mourisl@hotmail.com>
2023-07-27 01:11:05 -0400

    Fix a bug in SelectFa.pl

--- SelectFa.pl
+++ SelectFa.pl
@@ -16,7 +16,7 @@ while (<FP>)
 }
 close FP ;
 
-my $seq ;
+my $seq = "";
 my $header = "" ;
 while (<STDIN>)
 {
@@ -29,7 +29,7 @@ while (<STDIN>)
     }
 
     $header = $_ ;
-    $seq = 0 ;
+    $seq = "" ;
   }
   else
   {
